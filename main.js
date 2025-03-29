9946809bcc0c90e0c98dcf868d9ab2bebb9aa2d7
Tianpao <nxtianpao@163.com>
2025-03-29 16:08:18 +0800

    feat：模组自带信息判断

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ import fs from "fs";
 import toml from 'toml';
 import path from 'path';
 import pMap from "p-map";
-import {pino} from 'pino';
+import { pino } from 'pino';
 import { MultiBar } from "cli-progress";
 import ms from 'ms';
 
@@ -18,7 +18,7 @@ export async function DeEarthMain(modspath, movepath) {
     LOGGER.info(`DeEarth V${JSON.parse(fs.readFileSync("./package.json")).version}`)
     const resaddr = fs.readdirSync(modspath)
     LOGGER.info(`获取目录列表，一共${resaddr.length}个jar文件。`)
-    const totalBar = multibar.create(resaddr.length, 0, {filename: '总文件数'})
+    const totalBar = multibar.create(resaddr.length, 0, { filename: '总文件数' })
     for (let i = 0; i < resaddr.length; i++) {
         const e = `${modspath}/${resaddr[i]}`
         if (e.endsWith(".jar") && fs.statSync(e).isFile()) { //判断是否以.jar结尾并且是文件
@@ -33,10 +33,10 @@ export async function DeEarthMain(modspath, movepath) {
 export async function DeEarth(modpath, movepath) {
     const zip = new AdmZip(modpath).getEntries();
     //for (let i = 0; i < zip.length; i++) {
-        //const e = zip[i]
-        try { //Modrinth
-            for (let i = 0; i < zip.length; i++){
-                const e = zip[i]
+    //const e = zip[i]
+    try { //Modrinth
+        for (let i = 0; i < zip.length; i++) {
+            const e = zip[i]
             if (e.entryName == "META-INF/mods.toml") { //Forge,NeoForge
                 const modid = toml.parse(e.getData().toString('utf-8')).mods[0].modId
                 //const body = await got.get(`https://api.modrinth.com/v2/project/${modid}`, { headers: { "User-Agent": "DeEarth" } }).json()
@@ -53,67 +53,73 @@ export async function DeEarth(modpath, movepath) {
                 }
             }
         }
-        } catch (error) { //从Mixin判断 但是可能为不准确
-            for (let i = 0; i < zip.length; i++){
-                const e = zip[i]
+    } catch (error) { //mods.toml或fabric.mod.json判断
+        for (let i = 0; i < zip.length; i++) {
+            const e = zip[i]
             try {
-                if (!e.entryName.includes("/") && e.entryName.endsWith(".json") && !e.entryName.endsWith("refmap.json") && !e.entryName.endsWith("mod.json")) {
-                    LOGGER.info(e.entryName)
-                    const resx = JSON.parse(e.getData().toString('utf-8'))
-                    if (e.entryName.includes("common.mixins.json")) { //第一步从common mixins文件判断，判断失败后再使用modid.mixins.json进行判断
-                        //console.log(resx.mixins)
-                        //LOGGER.warn(resx.mixins+"我是傻逼"+typeof resx.mixins)
-                         /*if (resx.mixins == null || resx.mixins == "[]" && resx.client !== null || resx.client !== "[]") { //CM的判断if (反正对类型没严格要求，判断一下)
-                             fs.renameSync(modpath, `${movepath}/${path.basename(modpath)}`)
-                         }*/
-                        if(resx.mixins == null || Object.keys(resx.mixins).length == 0){
-                            //LOGGER.error(Object.keys(resx.mixins).length+"你好"+modpath+JSON.stringify(resx))
-                            //LOGGER.warn("客户端模组第一个"+modpath)
-                            fs.renameSync(modpath, `${movepath}/${path.basename(modpath)}`)
+                if (e.entryName == "META-INF/mods.toml") { //Forge,Neoforge
+                    const tr = toml.parse(e.getData().toString('utf-8'))
+                    const mcside = tr.dependencies[tr.mods[0].modId].find(mod => mod.modId === "minecraft").side
+                    const forgeside = tr.dependencies[tr.mods[0].modId].find(mod => mod.modId === "forge").side
+                    if (mcside == "CLIENT" || forgeside == "CLIENT") {
+                        fs.renameSync(modpath, `${movepath}/${path.basename(modpath)}`)
+                    }
+                } else if (e.entryName == "fabric.mod.json") { //Fabric
+                    const fmj = JSON.parse(e.getData().toString('utf-8')).environment
+                    if (fmj == "client") {
+                        fs.renameSync(modpath, `${movepath}/${path.basename(modpath)}`)
+                    }
+                }
+            } catch (erro) {//从Mixin判断 但是可能为不准确
+                for (let i = 0; i < zip.length; i++) {
+                    const e = zip[i]
+                    try {
+                        if (!e.entryName.includes("/") && e.entryName.endsWith(".json") && !e.entryName.endsWith("refmap.json") && !e.entryName.endsWith("mod.json")) {
+                            LOGGER.info(e.entryName)
+                            const resx = JSON.parse(e.getData().toString('utf-8'))
+                            if (e.entryName.includes("common.mixins.json")) { //第一步从common mixins文件判断，判断失败后再使用modid.mixins.json进行判断
+                                if (resx.mixins == null || Object.keys(resx.mixins).length == 0 && Object.keys(resx.client).length !== 0) {
+                                    fs.renameSync(modpath, `${movepath}/${path.basename(modpath)}`)
+                                }
+                            } else {
+                                if (resx.mixins == null || Object.keys(resx.mixins).length == 0 && Object.keys(resx.client).length !== 0) {
+                                    fs.renameSync(modpath, `${movepath}/${path.basename(modpath)}`)
+                                }
+                            }
                         }
-                    } else {
-                        //LOGGER.warn(resx.mixins+"我是傻逼第二个"+typeof resx.mixins)
-                        //if (resx.client !== null || resx.client !== "[]" && resx.mixins == null || resx.mixins == "[]") { //通过判断mixin来确定是否为客户端模组
-                            //fs.renameSync(modpath, `${movepath}/${path.basename(modpath)}`)
-                        //}
-                        //LOGGER.error(Object.keys(resx.mixins).length+"你好"+modpath+JSON.stringify(resx))
-                        if(resx.mixins == null || Object.keys(resx.mixins).length == 0 ){
-                            //LOGGER.warn("客户端模组"+modpath)
-                            fs.renameSync(modpath, `${movepath}/${path.basename(modpath)}`)
+                    } catch (err) {//避免有傻逼JSON写注释（虽然GSON可以这样 但是这样一点也不人道）
+                        if (err.errno !== -4058) {
+                            LOGGER.error(`大天才JSON写注释了估计，模组路径:${modpath}，过滤失败`)
                         }
                     }
                 }
-            } catch (err) {//避免有傻逼JSON写注释（虽然GSON可以这样 但是这样一点也不人道）
-                if(err.errno !==  -4058){
-                LOGGER.error(`大天才JSON写注释了估计，模组路径:${modpath}，过滤失败`)
-                }
             }
         }
-        }
+    }
     //}
 }
 
 async function FastGot(url) {
-    let e=[]
+    let e = []
     e.push([url])
-    const fastgot = await pMap(e,async(e)=>{
-try {
-    if(e[0] !== null){ //防止URL为空
-        //if(isChinaIpAddress((await got.get("https://4.ipw.cn/")).body)){
-    return (await got.get(e[0], { headers: { "User-Agent": "DeEarth" } })).body
-        //}else{
-    //return (await got.get(`https://mod.mcimirror.top/modrinth/${new URL(e[0]).pathname}`, { headers: { "User-Agent": "DeEarth" } })).body //MCIM源
-        //}
-    }
-} catch (error) {
-    if(error.message !== "Response code 404 (Not Found)"){
-        LOGGER.error({err:error})
-    }else{
-        throw new Error(error)
-    }
-}
-    },{
-        concurrency:48
+    const fastgot = await pMap(e, async (e) => {
+        try {
+            if (e[0] !== null) { //防止URL为空
+                //if(isChinaIpAddress((await got.get("https://4.ipw.cn/")).body)){
+                return (await got.get(e[0], { headers: { "User-Agent": "DeEarth" } })).body
+                //}else{
+                //return (await got.get(`https://mod.mcimirror.top/modrinth/${new URL(e[0]).pathname}`, { headers: { "User-Agent": "DeEarth" } })).body //MCIM源
+                //}
+            }
+        } catch (error) {
+            if (error.message !== "Response code 404 (Not Found)") {
+                LOGGER.error({ err: error })
+            } else {
+                throw new Error(error)
+            }
+        }
+    }, {
+        concurrency: 48
     })
     return fastgot[0]
 }
@@ -121,23 +127,23 @@ try {
 const LOGGER = pino({
     level: process.env.LOGLEVEL || 'info',
     transport: process.env.PLAIN_LOG
-      ? undefined
-      : {
-          target: 'pino-pretty',
-          options: {
-            translateTime: 'SYS:standard',
-            singleLine: true,
-          },
+        ? undefined
+        : {
+            target: 'pino-pretty',
+            options: {
+                translateTime: 'SYS:standard',
+                singleLine: true,
+            },
         },
-  })
+})
 
 const multibar = new MultiBar({
     format: ' {bar} | {filename} | {value}/{total}',
     noTTYOutput: true,
     notTTYSchedule: ms('10s'),
-  })
+})
 
 function isChinaIpAddress(ipAddress) {
-const chinaRegex = /^((?:(?:1(?:0|1|2[0-7]|[3-9][0-9])|2(?:[0-4][0-9]|5[0-5])|[3-9][0-9]{2})\.){3}(?:(?:1(?:0|1|2[0-7]|[3-9][0-9])|2(?:[0-4][0-9]|5[0-5])|[3-9][0-9]{2})))$/;
-return chinaRegex.test(ipAddress);
+    const chinaRegex = /^((?:(?:1(?:0|1|2[0-7]|[3-9][0-9])|2(?:[0-4][0-9]|5[0-5])|[3-9][0-9]{2})\.){3}(?:(?:1(?:0|1|2[0-7]|[3-9][0-9])|2(?:[0-4][0-9]|5[0-5])|[3-9][0-9]{2})))$/;
+    return chinaRegex.test(ipAddress);
 }
