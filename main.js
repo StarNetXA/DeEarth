5b788be49a3491dd782e9ee49f2ce4585724671a
Tianpao <nxtianpao@163.com>
2025-07-02 20:00:03 +0800

    fix：async error

--- main.js
+++ main.js
@@ -56,21 +56,21 @@ export async function DeEarth(modpath, movepath) {
     } catch (error) {
         try { //DeEarthPublic
             for (let i = 0; i < zip.length; i++) {
-            const e = zip[i]
-            if (e.entryName == "META-INF/mods.toml") { //Forge,Neoforge
-                const modid = toml.parse(e.getData().toString('utf-8')).mods[0].modId
-                const body = JSON.parse(FastGot(`https://dearth.0771010.xyz/api/modid?modid=${modid}`))
-                if (body.isClient) {
-                    fs.renameSync(modpath, `${movepath}/${path.basename(modpath)}`)
-                }
-            } else if (e.entryName == "fabric.mod.json") { //Fabric
-                const modid = JSON.parse(e.getData().toString('utf-8')).id
-                const body = JSON.parse(FastGot(`https://dearth.0771010.xyz/api/modid?modid=${modid}`))
-                if (body.isClient) {
-                    fs.renameSync(modpath, `${movepath}/${path.basename(modpath)}`)
+                const e = zip[i]
+                if (e.entryName == "META-INF/mods.toml") { //Forge,Neoforge
+                    const modid = toml.parse(e.getData().toString('utf-8')).mods[0].modId
+                    const body = JSON.parse(await FastGot(`https://dearth.0771010.xyz/api/modid?modid=${modid}`))
+                    if (body.isClient) {
+                        fs.renameSync(modpath, `${movepath}/${path.basename(modpath)}`)
+                    }
+                } else if (e.entryName == "fabric.mod.json") { //Fabric
+                    const modid = JSON.parse(e.getData().toString('utf-8')).id
+                    const body = JSON.parse(await FastGot(`https://dearth.0771010.xyz/api/modid?modid=${modid}`))
+                    if (body.isClient) {
+                        fs.renameSync(modpath, `${movepath}/${path.basename(modpath)}`)
+                    }
                 }
             }
-        }
         } catch (errorr) { //mods.toml或fabric.mod.json判断
             for (let i = 0; i < zip.length; i++) {
                 const e = zip[i]
